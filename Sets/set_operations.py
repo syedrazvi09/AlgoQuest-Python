2eae93d8c0866b18485d05bf3e136a7c3a176144
Syed Razvi <153203645+syedrazvi09@users.noreply.github.com>
2025-03-19 16:37:52 +0530

    Create set_operations.py

---
+++ Sets/set_operations.py
@@ -0,0 +1,12 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+union_set = A | B       
+intersection_set = A & B 
+difference_set = A - B   
+sym_diff_set = A ^ B     
+
+print("Union Set: ",union_set)
+print("Intersection Set: ",intersection_set)
+print("Difference Set: ",difference_set)
+print("Symmetric Difference Set: ",sym_diff_set)
