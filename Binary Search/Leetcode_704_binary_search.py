3b6282256e83ed126e1daf1c2d3fa8e506d4d9b4
Syed Razvi <razvisyed2002@gmail.com>
2026-07-07 15:44:57 +0530

    Solved leetcode 704 binary_search

---
+++ Binary Search/Leetcode_704_binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+nums = [2,5,7,9,11,15,23]
+target = 7
+print(binary_search(nums, target))
