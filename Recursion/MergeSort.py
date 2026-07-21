5c78bf73f9cf9d64e10b37c3101eadf30f0043a7
Syed Razvi <razvisyed2002@gmail.com>
2026-07-21 23:57:48 +0530

    solved merge sort using

---
+++ Recursion/MergeSort.py
@@ -0,0 +1,19 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[arr:])
+
+    merge(left, right)
+
+def merge(left, right):
+    res = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            res.append()
