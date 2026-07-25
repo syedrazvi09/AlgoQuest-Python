9335f2e2fbf7d0000a4a0d34b4622e7ad097dfbe
Syed Razvi <razvisyed2002@gmail.com>
2026-07-25 14:23:53 +0530

    implemented preorder traversal using recursion

---
+++ Trees/Preorder_traversal.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.val)
+
+
+A = TreeNode(1)
+B = TreeNode(2)
+C = TreeNode(3)
+D = TreeNode(4)
+E = TreeNode(5)
+F = TreeNode(10)
+
+
+A.left = B
+A.right = C
+B.left = D
+B.right = E
+C.left = F
+
+
+def pre_order(node):
+    if not node:
+        return
+
+    print(node)
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+pre_order(A)
