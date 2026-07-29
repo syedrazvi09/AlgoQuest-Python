7d528f2db7b95fd806e40b02c371410d82eb3bf8
Syed Razvi <razvisyed2002@gmail.com>
2026-07-29 14:44:50 +0530

    solved leetcode 424

--- Dynamic Programming/Leetcode_139.py
+++ Dynamic Programming/Leetcode_139.py
@@ -3,7 +3,7 @@ def wordbreak(s, word):
     dp = [False] * (len(s) + 1)
     dp[len(s)] = True
 
-    for i in range(len(s) -1, -1, -1):
+    for i in range(len(s) - 1, -1, -1):
         for w in word:
             if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
                 dp[i] = dp[i + len(w)]
--- Trees/practice.py
+++ Trees/practice.py
@@ -1,15 +1,16 @@
-def rob(nums):
-    prof1 = 0
-    prof2 = 0
+def twoSum(numbers, target):
+    seen = {}
 
-    for i in range(0, len(nums), 2):
-        prof1 += nums[i]
+    for key, val in enumerate(numbers):
+        goal = target - val
+        if goal in seen:
+            return [seen[goal] + 1, key + 1]
+        seen[val] = key
 
-    for i in range(1, len(nums), 2):
-        prof2 += nums[i]
 
-    print(prof1)
-    print(prof2)
+nums = [2,7,11,15]
+print(twoSum(nums, 9))
 
-arr = [1,2,3,1]
-rob(arr)
+
+
+
