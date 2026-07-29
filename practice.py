547c487e3f178dd14548f38ffa54b0c436f30c81
Syed Razvi <razvisyed2002@gmail.com>
2026-07-29 19:18:11 +0530

    solved leetcode 143

---
+++ practice.py
@@ -0,0 +1,47 @@
+class SinglyNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+
+head = SinglyNode(1)
+A = SinglyNode(3)
+B = SinglyNode(5)
+C = SinglyNode(7)
+
+head.next = A
+A.next = B
+B.next = C
+
+
+
+cur = head
+
+while cur:
+    print(cur)
+    cur = cur.next
+
+
+def display(head):
+    cur = head
+    elements = []
+    while cur:
+        elements.append(str(cur.val))
+        cur = cur.next
+    print(' -> '.join(elements))
+
+display(head)
+
+
+def search(head, target):
+    cur = head
+    while cur:
+        if cur.val == target:
+            return True
+        cur = cur.next
+    return False
+
+print(search(head, 2))
