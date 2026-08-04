7f17b260ab1e9f61dc6cf56ad7be4b22644e6994
Syed Razvi <razvisyed2002@gmail.com>
2026-08-04 20:19:26 +0530

    solved leetcode 930

---
+++ Sliding Window/Leetcode_930.py
@@ -0,0 +1,18 @@
+def nsws(nums, goal):
+
+    def helper(x):
+        if x < 0:
+            return 0
+
+        res = 0
+        cur = 0
+        l = 0
+        for r in range(len(nums)):
+            cur += nums[r]
+            while cur > x:
+                cur -= nums[l]
+                l += 1
+            res += (r - l + 1)
+        return res
+
+    return helper(goal) - helper(goal - 1)
