c402d9273fc4f1e8f7ecd30de1ffbf847d7e3029
Syed Razvi <razvisyed2002@gmail.com>
2026-08-05 16:09:13 +0530

    solved leetcode 1094

---
+++ Difference Array or Range Updates/Leetcode_1094.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def carPooling(trips, capacity):
+    trips.sort(key = lambda t: t[1])
+
+    minHeap = []
+    curPass = 0
+
+    for t in trips:
+        numPass, start, end = t
+        while minHeap and minHeap[0][0] <= start:
+            curPass -= minHeap[0][1]
+            heapq.heappop(minHeap)
+
+        curPass += numPass
+        if curPass >= capacity:
+            return False
+        heapq.heappush(minHeap, [end, numPass])
+    return True
